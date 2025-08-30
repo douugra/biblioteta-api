d6a5ff24efef5e09cb97266d446502d61a6b4eb7
douugra <douglas11918@gmail.com>
2025-08-30 11:55:22 -0300

    Update index.py
    atualizando

--- biblioteca-api/api/index.py
+++ biblioteca-api/api/index.py
@@ -1,5 +1,9 @@
 import sqlite3
 from flask import Flask, jsonify, request, g
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app, origins=["https://biblioteca-front-pb4qaj6yv-douglas-projects-9825d618.vercel.app/"])
+
 
 app = Flask(__name__)
 DATABASE = "biblioteca.db"
